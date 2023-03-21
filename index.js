d6659227292f08a00783cd3ed4dae8626b45cd34
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-03-22 01:28:14 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name, email) {
+	return fetch('http://localhost:3000/users', {
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json",
+		},
+		method: "POST",
+		body: JSON.stringify({name, email})
+	}).then(res => res.json() ).then(data => {
+		const p = document.createElement('p');
+		p.textContent = `id: ${data.id}`;
+		const users = document.querySelector('div#users');
+		console.log(users);
+		users.appendChild(p);
+	}).catch(({message}) => {
+		alert(`Error! ${message}`)
+	})
+}
+
+submitData("abdullah", "abdullah.binsaadd@gmail.com")
